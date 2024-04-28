2f73c2f7972299832a0be0f4fb62671232d25981
smitapatel12 <smita.mgs1215@gmail.com>
2024-04-28 18:28:00 +0530

    hj

---
+++ currencyConvertor/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   <h1 className='bg-orange-500'>currency convertor</h1>
+    </>
+  )
+}
+
+export default App
